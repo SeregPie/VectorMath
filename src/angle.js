1c3f44ef13eb1f84040515467cbaf58aaf585f3c
SeregPie <seregpie@gmail.com>
2020-03-25 16:09:52 +0100

    use Math.clamp

--- src/angle.js
+++ src/angle.js
@@ -1,3 +1,5 @@
+import Math_clamp from './core/Math/clamp';
+
 import dot from './dot';
 import length from './length';
 
@@ -5,7 +7,7 @@ export default function(vector, otherVector) {
 	let denominator = length(vector) * length(otherVector);
 	if (denominator) {
 		let numerator = dot(vector, otherVector);
-		return Math.acos(Math.min(Math.max(numerator / denominator, -1), 1));
+		return Math.acos(Math_clamp(numerator / denominator, -1, 1));
 	}
 	return Math.PI / 2;
 }
--- src/pearsonCorrelationCoefficient.js
+++ src/pearsonCorrelationCoefficient.js
@@ -1,3 +1,4 @@
+import Math_clamp from './core/Math/clamp';
 import Math_standardDeviation from './core/Math/standardDeviation';
 
 import covariance from './covariance';
@@ -6,7 +7,7 @@ export default function(vector, otherVector) {
 	let denominator = Math_standardDeviation(...vector) * Math_standardDeviation(...otherVector);
 	if (denominator) {
 		let numerator = covariance(vector, otherVector);
-		return Math.min(Math.max(numerator / denominator, -1), 1);
+		return Math_clamp(numerator / denominator, -1, 1);
 	}
 	return 0;
 }
