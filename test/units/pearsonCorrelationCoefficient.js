084c58be0b58e40cf149d5e0596533ae8054f8c6
Sergej Sintschilin <seregpie@gmail.com>
2020-07-24 19:32:37 +0200

    fix precision

--- test/units/pearsonCorrelationCoefficient.js
+++ test/units/pearsonCorrelationCoefficient.js
@@ -4,6 +4,6 @@ let VectorMath = require('../../index');
 
 module.exports = function() {
 	assert.equal(VectorMath.PearsonCorrelationCoefficient([], []), 0);
-	assert.equal(VectorMath.PearsonCorrelationCoefficient([-33, -23, 91, 71], [-33, -23, 91, 71]), 1);
+	assert.equal(VectorMath.PearsonCorrelationCoefficient([-71, 8, -83, 53], [-71, 8, -83, 53]), 1);
 	assert.almostEqual(VectorMath.PearsonCorrelationCoefficient([90, 21, -63, -99], [94, 36, -100, 40]), 0.590648646);
 };
