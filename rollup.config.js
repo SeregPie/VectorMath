25a5892d42e42653095e1697cae19ac8b179f45b
Sergej Sintschilin <seregpie@gmail.com>
2020-07-24 14:03:54 +0200

    remove buble

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,10 @@
 import {terser} from 'rollup-plugin-terser';
-import buble from '@rollup/plugin-buble';
 
 import {main} from './package.json';
 
 export default {
 	input: './src/index.js',
 	plugins: [
-		buble(),
 		terser(),
 	],
 	output: {
