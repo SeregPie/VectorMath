256a6dcdc162abc0f0cb2977d3ffbc1868d3e848
Sergej Sintschilin <seregpie@gmail.com>
2020-08-03 16:11:46 +0200

    use babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,15 @@
 import {terser} from 'rollup-plugin-terser';
+import babel from '@rollup/plugin-babel';
 
 import {main} from './package.json';
 
 export default {
 	input: './src/index.js',
 	plugins: [
+		babel({
+			babelHelpers: 'bundled',
+			presets: ['@babel/preset-env'],
+		}),
 		terser(),
 	],
 	output: {
